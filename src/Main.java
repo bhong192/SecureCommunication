e1ed7f7f94a5295c97073a3adff5ff51ac3b0bd8
Bryant Hong <bhong4059@gmail.com>
2022-11-22 00:08:08 -0800

    Added file writing for transmission between parties

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import javax.crypto.*;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 
 
 public class Main {
@@ -10,27 +11,35 @@ public class Main {
 
         // STEP 1: Generate RSA key pairs for both receiver and sender
         Receiver receiver = new Receiver();
-        //receiver.createKeyPair(receiver.privateKey, receiver.publicKey);
         receiver.createKeyPair();
 
         Sender sender = new Sender();
-        //sender.createKeyPair(sender.privateKey, sender.publicKey);
+        sender.createKeyPair();
 
         // create AES key (symmetric so only one needed for both parties)
         SecretKey aesKey = Communicator.generateAESkey();
-        //System.out.println(aesKey);
 
         // STEP 2: Encrypt each person's message using the AES key
         String receiverEncryptedMessage = receiver.encryptMessage("receiver.txt", aesKey);
         String senderEncryptedMessage = sender.encryptMessage("sender.txt", aesKey);
 
-        // STEP 3: Encrypt the AES key using RSA private key of the sender
+        // STEP 3: Create MAC
+
+        // STEP 4: Write public AES and RSA keys to transmitted_data file
+        try{
+            FileWriter fileWriter = new FileWriter("transmission.txt");
+            String content = receiver.publicKey.toString() + "\n" + sender.publicKey.toString() + "\n" + senderEncryptedMessage;
+            fileWriter.write(content);
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // STEP 5: Encrypt the AES key using RSA private key of the sender
         String encryptedAESKEY = sender.encryptAESKey(aesKey, receiver.publicKey);
         System.out.println(encryptedAESKEY);
 
-        // STEP 4: Concatenate the encrypted message and AES key then send to receiver
-
-
+        // STEP 6: Concatenate the encrypted message and AES key then send to receiver
 
 
     }
