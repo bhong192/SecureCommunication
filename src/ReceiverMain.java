973c66146043cee7b4d5e5ab87643112cc488bbb
Bryant Hong <bhong4059@gmail.com>
2022-12-01 23:43:21 -0800

    Finished implementing two main methods

--- src/ReceiverMain.java
+++ src/ReceiverMain.java
@@ -34,28 +34,22 @@ public class ReceiverMain {
 
             // remove last character due to encoding error (\n)
             readEncryptedMessage = readEncryptedMessage.substring(0, readEncryptedMessage.length()-1);
-            //readEncryptedKey = readEncryptedKey.substring(0, readEncryptedKey.length()-1);
 
             // decrypt message using the AES key
-//            // TODO: read receiver's private key from receiverPrivateKey.priv
-//            File privateKeyFile = new File("receiverPrivateKey.priv");
-//            byte[] privateKeyBytes = Files.readAllBytes(privateKeyFile.toPath());
-//            KeyFactory privateKeyFactory = KeyFactory.getInstance("RSA");
-//            EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
-//            PrivateKey receiverPrivateKey = privateKeyFactory.generatePrivate(privateKeySpec);
-//            System.out.println("Private key size: " + receiverPrivateKey.getEncoded().length);
-            System.out.println("readEncryptedKey: " + readEncryptedKey);
-            System.out.println("receiverPrivateKey: " + receiverPrivateKey);
-            SecretKey recoveredAesKey = Communicator.decryptRSA(readEncryptedKey, receiverPrivateKey);
-            String decryptedMessage = Communicator.decrypt(readEncryptedMessage, recoveredAesKey); // aesKey here should be obtained after decrypting it with private key instead
+            // TODO: write function to decrypt the AES key using Receiver's private key and using that result to decrypt the message
+//            SecretKey recoveredAesKey = Communicator.decryptRSA(readEncryptedKey, receiver.privateKey); // original
+            String recoveredAesKey = Communicator.decryptRsaMessage(readEncryptedKey, receiverPrivateKey);
+
+            String decryptedMessage = Communicator.decryptAES(readEncryptedMessage, recoveredAesKey);
+//            String decryptedMessage = sender.decrypt(readEncryptedMessage, recoveredAesKey); // aesKey here should be obtained after decrypting it with private key instead
 
             // verify MAC by recalculating it from the message and comparing it to what was sent
-            // TODO: Decrypt MAC key and recalculate to verify message integrity
             SecretKey recoveredMacKey = Communicator.decryptRSA(readMacKey, receiverPrivateKey);
-            Mac mac = Mac.getInstance("HmacMD5");
-            mac.init(recoveredMacKey);
+            Mac verificationMAC = Mac.getInstance("HmacMD5");
+            verificationMAC.init(recoveredMacKey);
+
             byte[] decryptedMessageBytes = decryptedMessage.getBytes();
-            byte[] recalculatedMAC = mac.doFinal(decryptedMessageBytes);
+            byte[] recalculatedMAC = verificationMAC.doFinal(decryptedMessageBytes);
             String recalculatedMACString = new String(recalculatedMAC);
 
             // STEP 7: Print status of verifications (intact message and valid MAC)
@@ -65,6 +59,46 @@ public class ReceiverMain {
         catch(Exception e){
             e.printStackTrace();
         }
+
+//        try{
+//            String readEncryptedMessage = Files.readAllLines(Paths.get("src/resources/transmission.txt")).get(0);
+//            String readEncryptedKey = Files.readAllLines(Paths.get("src/resources/transmission.txt")).get(1);
+//            String readMAC = Files.readAllLines(Paths.get("src/resources/transmission.txt")).get(2);
+//            String readMacKey = Files.readAllLines(Paths.get("src/resources/transmission.txt")).get(3);
+//
+//            // remove last character due to encoding error (\n)
+//            readEncryptedMessage = readEncryptedMessage.substring(0, readEncryptedMessage.length()-1);
+//            //readEncryptedKey = readEncryptedKey.substring(0, readEncryptedKey.length()-1);
+//
+//            // decrypt message using the AES key
+////            // TODO: read receiver's private key from receiverPrivateKey.priv
+////            File privateKeyFile = new File("receiverPrivateKey.priv");
+////            byte[] privateKeyBytes = Files.readAllBytes(privateKeyFile.toPath());
+////            KeyFactory privateKeyFactory = KeyFactory.getInstance("RSA");
+////            EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+////            PrivateKey receiverPrivateKey = privateKeyFactory.generatePrivate(privateKeySpec);
+////            System.out.println("Private key size: " + receiverPrivateKey.getEncoded().length);
+//            System.out.println("readEncryptedKey: " + readEncryptedKey);
+//            System.out.println("receiverPrivateKey: " + receiverPrivateKey);
+//            SecretKey recoveredAesKey = Communicator.decryptRSA(readEncryptedKey, receiverPrivateKey);
+//            String decryptedMessage = Communicator.decrypt(readEncryptedMessage, recoveredAesKey); // aesKey here should be obtained after decrypting it with private key instead
+//
+//            // verify MAC by recalculating it from the message and comparing it to what was sent
+//            // TODO: Decrypt MAC key and recalculate to verify message integrity
+//            SecretKey recoveredMacKey = Communicator.decryptRSA(readMacKey, receiverPrivateKey);
+//            Mac mac = Mac.getInstance("HmacMD5");
+//            mac.init(recoveredMacKey);
+//            byte[] decryptedMessageBytes = decryptedMessage.getBytes();
+//            byte[] recalculatedMAC = mac.doFinal(decryptedMessageBytes);
+//            String recalculatedMACString = new String(recalculatedMAC);
+//
+//            // STEP 7: Print status of verifications (intact message and valid MAC)
+//            System.out.println("Decrypted Message: " + decryptedMessage);
+//            System.out.println("Valid MAC Status: " + recalculatedMACString.equals(readMAC));
+//        }
+//        catch(Exception e){
+//            e.printStackTrace();
+//        }
 
 
     }
